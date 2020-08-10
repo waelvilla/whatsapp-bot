a8502cb008bb2bb60610b0c5c15e093ec9e9ea6b
waelvilla <waelvilla@gmail.com>
2020-08-10 20:56:16 +0300

    added sending images

--- src/selenium.js
+++ src/selenium.js
@@ -49,6 +49,13 @@ async function getMessage(){
     })
   })}
 
+async function copy(driver) {
+
+}
+async function paste(driver) {
+  await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(Key.chord(Key.CONTROL, 'v'));
+}
+
 async function loadCookies(driver){
     const cookies = await getCookies();
     for(var cookie of cookies){
@@ -65,8 +72,9 @@ async function saveCookies(cookies){
   });
 }
 
-async function sendMessage({driver, actions, phone, message}){
+async function sendMessage({driver, phone, message}){
   const encoded = encodeURIComponent(message)
+  // const encoded=''
   const url = `https://api.whatsapp.com/send?phone=${phone}&text=${encoded}`;
   await driver.get(url);
   await driver.wait(until.elementIsVisible(driver.findElement(By.id('action-button'))))
@@ -77,15 +85,22 @@ async function sendMessage({driver, actions, phone, message}){
   // await driver.wait(until.elementLocated(driver.findElement(By.className('NVQmc'))))
   try{ 
     try{
-    await sleep(5000)
-    // _1U1xa
-    await (await driver.findElement(By.className('_1U1xa'))).click()
-    // await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(message, Key.RETURN);
+    await sleep(6000)
+    console.log('pasting image');
+    await sleep(1000)
+    await paste(driver)
+    console.log('clicking send image');
+    await (await driver.findElement(By.className('_3y5oW _3qMYG'))).click() //click send image
+    // await (await driver.findElement(By.className('_1U1xa'))).click() //click send text
+    await sleep(500)
+    // await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(message, Key.RETURN); //clicks "Enter" to send text
   } catch(e){
     console.log('not logged in');
     await sleep(5000)
-    await (await driver.findElement(By.className('_1U1xa'))).click()
-      // await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(message, Key.RETURN);
+    console.log('clicking send image');
+    await (await driver.findElement(By.className('_3y5oW _3qMYG'))).click() //click send image
+    // await (await driver.findElement(By.className('_1U1xa'))).click() //click send text
+      // await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(message, Key.RETURN); //clicks "Enter" to send text
     }
   } catch(e){
     console.log('failed to send message');
@@ -95,23 +110,24 @@ async function sendMessage({driver, actions, phone, message}){
 (async function run() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    const actions = driver.actions()
     const phones = await getPhones();
     const message = await getMessage()
-    actions.sendKeys(Key.chord(Key.CONTROL, "p")).perform()
+    // const message = ''
+    // await driver.actions().keyDown(Key.CONTROL).sendKeys('a').perform();
     let i=1 
-    // for(const user of phones){
-    //   const {phone, gender} = user
-    //   console.log(`run ${i}: ${phone}`);
-    //   // let message = await getMessage()
-    //   try{
-    //   await sendMessage({driver, actions, phone, message})
-    //   } catch(e){
-    //     console.log('failed to send to ', phone);
-    //   }
-    //   i++
-    //   await sleep(10000)
-    // }
+    for(const user of phones){
+      const {phone, gender} = user
+      console.log(`run ${i}: ${phone}`);
+      // let message = await getMessage()
+      try{
+      await sendMessage({driver, phone, message})
+      } catch(e){
+        console.log(e);
+        console.log('failed to send to ', phone);
+      }
+      i++
+      await sleep(10000)
+    }
     await sleep(5000)
     console.log('done');
   } finally {

f85c8dba8653bc6df0271407e55e697aa80231a8
waelvilla <waelvilla@gmail.com>
2020-09-06 14:27:30 +0300

    added csv phones

--- src/selenium.js
+++ src/selenium.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const csv = require('csv-parser');
 const {Builder, By, Key, until} = require('selenium-webdriver');
  
 const sleep = async (time) => {
@@ -9,9 +10,9 @@ const sleep = async (time) => {
   })
 }
 
-async function getPhones(){
+async function getPhonesText(){
   return new Promise((resolve, reject)=> {
-    fs.readFile('./phones.json', (err, data) => {
+    fs.readFile('./old-phones.json', (err, data) => {
       if(err){
         console.log('err', err);
         reject(err)
@@ -20,6 +21,22 @@ async function getPhones(){
       const parsed = JSON.parse(data)
       resolve(parsed)
     })
+  })
+}
+
+async function getPhonesCSV(){
+  return new Promise((resolve, reject) => {
+      const data = []
+      fs.createReadStream('phones.csv')
+      .pipe(csv({separator: '\t'}))
+      .on('data', (row) => {
+          data.push(row)
+      })
+      .on('end', () => {
+          resolve(data);
+      }).on('error', (err) => {
+          reject(err)
+      });
   })
 }
 
@@ -72,9 +89,36 @@ async function saveCookies(cookies){
   });
 }
 
+async function sendImageMessage({driver, phone, message}){
+  const encoded = encodeURIComponent(message)
+  const url = `https://api.whatsapp.com/send?phone=${phone}&text=${encoded}`;
+  await driver.get(url);
+  await driver.wait(until.elementIsVisible(driver.findElement(By.id('action-button'))))
+  await (await driver.findElement(By.id('action-button'))).click()
+  await sleep(2000)
+  await (await driver.findElement(By.partialLinkText('use WhatsApp Web'))).click()
+  console.log('starting 5');
+  try{ 
+    try{
+    await sleep(6000)
+    console.log('pasting image');
+    await sleep(1000)
+    await paste(driver)
+    console.log('clicking send image');
+    await (await driver.findElement(By.className('_3y5oW _3qMYG'))).click() //click send image
+  } catch(e){
+    console.log('not logged in');
+    await sleep(5000)
+    console.log('clicking send image');
+    await (await driver.findElement(By.className('_3y5oW _3qMYG'))).click() //click send image
+    }
+  } catch(e){
+    console.log('failed to send image to ', phone);
+  }
+}
+
 async function sendMessage({driver, phone, message}){
   const encoded = encodeURIComponent(message)
-  // const encoded=''
   const url = `https://api.whatsapp.com/send?phone=${phone}&text=${encoded}`;
   await driver.get(url);
   await driver.wait(until.elementIsVisible(driver.findElement(By.id('action-button'))))
@@ -86,31 +130,24 @@ async function sendMessage({driver, phone, message}){
   try{ 
     try{
     await sleep(6000)
-    console.log('pasting image');
-    await sleep(1000)
-    await paste(driver)
-    console.log('clicking send image');
-    await (await driver.findElement(By.className('_3y5oW _3qMYG'))).click() //click send image
-    // await (await driver.findElement(By.className('_1U1xa'))).click() //click send text
+    await (await driver.findElement(By.className('_1U1xa'))).click() //click send text
     await sleep(500)
     // await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(message, Key.RETURN); //clicks "Enter" to send text
   } catch(e){
     console.log('not logged in');
     await sleep(5000)
-    console.log('clicking send image');
-    await (await driver.findElement(By.className('_3y5oW _3qMYG'))).click() //click send image
-    // await (await driver.findElement(By.className('_1U1xa'))).click() //click send text
+    await (await driver.findElement(By.className('_1U1xa'))).click() //click send text
       // await driver.findElement(By.className('_2FVVk _2UL8j')).sendKeys(message, Key.RETURN); //clicks "Enter" to send text
     }
   } catch(e){
-    console.log('failed to send message');
+    console.log('failed to send message to', phone);
   }
 }
 
 (async function run() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    const phones = await getPhones();
+    const phones = await getPhonesCSV();
     const message = await getMessage()
     // const message = ''
     // await driver.actions().keyDown(Key.CONTROL).sendKeys('a').perform();
@@ -120,13 +157,13 @@ async function sendMessage({driver, phone, message}){
       console.log(`run ${i}: ${phone}`);
       // let message = await getMessage()
       try{
-      await sendMessage({driver, phone, message})
+      // await sendMessage({driver, phone, message})
       } catch(e){
         console.log(e);
         console.log('failed to send to ', phone);
       }
       i++
-      await sleep(10000)
+      await sleep(1000)
     }
     await sleep(5000)
     console.log('done');
